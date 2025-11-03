a5049c830d910ed1d7d91be50fa3798de54d4e16
Xaeka <luminusmith@gmail.com>
2025-11-03 22:58:21 +0800

    ①【Java】认识变量及其三个基本要素

---
+++ basic-learn/src/main/java/com/taxas/xaeka/edu/chapter03/quiz/quiz01.java
@@ -0,0 +1,19 @@
+package com.taxas.xaeka.edu.chapter03.quiz;
+
+/**
+ * <p>变量</p>
+ * <p>三个基本要素： 类型 + 名称 +值</p>
+ *
+ * @author Xaeka
+ * @version 1.0
+ * @since 2025-11-03 17:25:26
+ */
+
+public class quiz01 {
+    // 先写main方法
+    public static void main(String[] args){
+        int a;
+        a =10086;
+        System.out.println(a);
+    }
+}
