51f8eb49e9a79a06378c427c0850d483a7c08a78
Xaeka <luminusmith@gmail.com>
2025-11-15 00:42:43 +0800

    ①【Java】初步了解并运用 关系运算符，同时了解并运用 逻辑运算符 的逻辑与(&)、逻辑或(|)、短路与(&&)、短路或(||) ②【Episode】065-072

---
+++ basic-learn/src/main/java/com/taxas/xaeka/edu/chapter04/LogicOperator02.java
@@ -0,0 +1,64 @@
+package com.taxas.xaeka.edu.chapter04;
+
+/**
+ * <p>逻辑运算符02 - 逻辑或| 跟 短路或|| 的使用</p>
+ * <ul>
+ *   <li>a&b: & 叫做 逻辑与  -> 当a和b同时为true，则结果为true，否则为false</li>
+ *   <li>a&&b: && 叫做 短路与 -> 当a和b同时为true，则结果为true，否则为false</li>
+ *   <li>a|b: | 叫做 逻辑或 -> 在a或b当中，任意一个为true，则结果为true，否则为false</li>
+ *   <li>a||b: || 叫做 短路或 -> 在a或b当中，任意一个为true，则结果为true，否则为false</li>
+ *   <li>!a: ! 叫做 取反/非运算) -> 当a为true，则结果为false，当a为false时，结果为true（总之就是结果和本来的值相反就对了！）</li>
+ *   <li>a^b: ^ 叫做 逻辑异或 -> 当a和b不同时，则结果为true，否则为false</li>
+ * </ul>
+ *
+ * @author Xaeka
+ * @version 1.0
+ * @since 2025-11-14 16:17:44
+ */
+
+public class LogicOperator02 {
+    public static void main(String[] args) {
+        // 短路或 || 的使用
+        int minScore = 20;
+        int maxScore = 80;
+        // 若最小分数大于10且最大分数小于100，那么输出“优良”，否则输出“不合格”。
+        if (minScore > 10 || maxScore < 78) {
+            System.out.println("优良");
+        } else {
+            System.out.println("不合格");
+        }
+
+        // 逻辑或 | 的使用
+        int minScore1 = 20;
+        int maxScore1 = 80;
+        // 若最小分数大于10且最大分数小于100，那么输出“优良”，否则输出“不合格”。
+        if (minScore1 > 10 | maxScore1 < 78) {
+            System.out.println("优良");
+        } else {
+            System.out.println("不合格");
+        }
+
+        // 短路,视情况,优先判断左边,可以提前结束;逻辑,左右两边都判断
+        // 1.|| 短路或：如果第一个条件为true，则第二个条件不会判断，最终结果为true，效率高
+        int a = 9;
+        int b = 99;
+        if (a < 10 || ++b > 50) {   // 只执行到 a < 10, 后面的++b不执行，仍为99
+            System.out.println("成立");
+        } else {
+            System.out.println("不成立");
+        }
+        System.out.println("此时的a值为：" + a + ", b值为：" + b);   // a = 9, b = 99
+
+        // 2.| 逻辑或：不管第一个条件是否为false，第二个条件都要判断(也就是说都要执行)，最终结果为效率低
+        int c = 9;
+        int d = 99;
+        if (c < 10 | ++d > 50) { // 首先判断c < 10 成立，然后继续自行++d > 50,即此时b为100
+            System.out.println("成立");   //
+        } else {
+            System.out.println("不成立");
+        }
+        System.out.println("此时的c值为：" + c + ", d值为：" + d);   // a = 9, b = 100
+
+        // 3，开发中，我们使用的基本是短路与 &&，效率高
+    }
+}
---
+++ basic-learn/src/main/java/com/taxas/xaeka/edu/chapter04/RelationalOperator.java
@@ -0,0 +1,31 @@
+package com.taxas.xaeka.edu.chapter04;
+
+/**
+ * <p>关系运算符 - 案例演示</p>
+ * <ul>
+ *   <li>关系运算符的结果都是boolean类型，也就是要么是true，要么是false</li>
+ *   <li>关系运算符组成的表达式称为<b>关系表达式</b>，类似于a > b</li>
+ *   <li>比较运算符 "==" 绝对不能写成赋值符号 "=" </li>
+ * </ul>
+ *
+ * @author Xaeka
+ * @version 1.0
+ * @since 2025-11-14 15:14:02
+ */
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println(a > b);  // true
+        System.out.println(a >= b); // true
+        System.out.println(a <= b); // false
+        System.out.println(a < b);  // false
+        System.out.println(a == b);  // false
+        System.out.println(a != b); // true
+        boolean flag = a > b;       // true
+        System.out.println("flag:" + flag);   // true
+
+    }
+}
+
